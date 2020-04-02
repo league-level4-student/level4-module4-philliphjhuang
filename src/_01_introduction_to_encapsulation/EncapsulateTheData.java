43edca95caabf6c8394fd9f038a399a7aa0b0bb6
philliphjhuang <30792325+philliphjhuang@users.noreply.github.com>
2020-04-01 18:26:23 -0700

    working on polymorphs

--- src/_01_introduction_to_encapsulation/EncapsulateTheData.java
+++ src/_01_introduction_to_encapsulation/EncapsulateTheData.java
@@ -7,7 +7,7 @@ package _01_introduction_to_encapsulation;
  * To encapsulate a member variable, follow these three steps:
  * 
  * 1. Make the member variable private (or protected)
- * 
+ *
  * 2. Make a getter and setter method (accessor and mutator) for the variable.
  * 
  * 3. Add restrictions to the setter method so the member variable cannot be 
@@ -23,12 +23,50 @@ public class EncapsulateTheData {
 	//2. Create a new JUnit Test case and write tests to verify that 
 	//   the member variables' getters and setters are working
 	
-	int itemsReceived; //must not be negative. All negative arguments get set to 0.
-	float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
-	String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
-	Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
+	private int itemsReceived; //must not be negative. All negative arguments get set to 0.
+	private float degreesTurned; //must be locked between 0.0 and 360.0 inclusive.
+	private String nomenclature = " "; //must not be set to a blank string. Blank Strings get set to a space
+	private Object memberObj;  //must not be a String.  If it is a String, set it equal to a new Object();
 	
 	public static void main(String[] args) {
 		
+	}
+	public void setItemsReceived(int a) {
+		if(a < 0) {
+			a = 0;
+		}
+		itemsReceived = a;
+	}
+	public int getItemsReceived() {
+		return itemsReceived;
+	}
+	
+	public void setDegreesTurned(float a) {
+		if(a >= 0 && a <= 360) {
+			degreesTurned = a;
+		}
+	}
+	public float getDegreesTurned() {
+		return degreesTurned;
+	}
+	
+	public void setNomenclature(String a) {
+		if(a.equals("")){
+			a = " ";
+		}
+		nomenclature = a;
+	}
+	public String getNomenclature() {
+		return nomenclature;
+	}
+	
+	public void getMemberObj(Object a) {
+		if(a.getClass().equals(String.class)) {
+			a = new Object();
+		}
+		memberObj = a;
+	}
+	public Object setMemberObj() {
+		return memberObj;
 	}
 }
--- src/_03_polymorphs/Polymorph.java
+++ src/_03_polymorphs/Polymorph.java
@@ -3,12 +3,13 @@ package _03_polymorphs;
 import java.awt.Graphics;
 
 public abstract class Polymorph {
-    int x;
-    int y;
-    
+    private int x;
+    private int y;
+    private int width = 50;
+    private int height = 50;
     Polymorph(int x, int y){
-   	 this.x = x;
-   	 this.y = y;
+    	this.x = x;
+    	this.y = y;
     }
     
     public void update(){
@@ -16,4 +17,32 @@ public abstract class Polymorph {
     }
     
     public abstract void draw(Graphics g);
+    
+    public void setX(int x) {
+    	this.x = x;
+    }
+    public int getX() {
+    	return x;
+    }
+    
+    public void setY(int y) {
+    	this.y = y;
+    }
+    public int getY() {
+    	return y;
+    }
+    
+    public void setWidth(int width) {
+    	this.width = width;
+    }
+    public int getWidth() {
+    	return width;
+    }
+    
+    public void setHeight(int height) {
+    	this.height = height;
+    }
+    public int getHeight() {
+    	return height;
+    }
 }
