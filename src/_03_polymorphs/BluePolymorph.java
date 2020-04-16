db94dbe99831c3a18a38af0d7564b054bddc05c9
philliphjhuang <30792325+philliphjhuang@users.noreply.github.com>
2020-04-15 17:50:10 -0700

    finished

--- src/_03_polymorphs/BluePolymorph.java
+++ src/_03_polymorphs/BluePolymorph.java
@@ -2,7 +2,8 @@ package _03_polymorphs;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.util.Random;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 
 public class BluePolymorph extends Polymorph{
 
@@ -56,10 +57,7 @@ class MovingMorph extends Polymorph{
 }
 
 class CircleMorph extends Polymorph{
-	
-	int f = 1;
-	int r = 100;
-	
+	int counter = 0;
 	CircleMorph(int x, int y, int width, int height) {
 		super(x, y, width, height);
 	}
@@ -71,8 +69,42 @@ class CircleMorph extends Polymorph{
 	}
 	
 	public void update() {
-		setX(((int)Math.cos(f)*r)+100);
-		setY(((int)Math.sin(f)*r)+100);
-		f++;
+		setX((int) (250*Math.cos(counter/4)+225));
+		setY((int) (250*Math.sin(counter/4)+225));
+		counter++;
 	}
 }
+
+class MouseMorph extends Polymorph implements MouseMotionListener{
+	MouseMorph(int x, int y, int width, int height) {
+		super(x, y, width, height);
+	}
+	
+	@Override
+	public void draw(Graphics g) {
+		g.setColor(Color.orange);
+		g.fillRect(getX(), getY(), getWidth(), getHeight());
+	}
+	
+	public void update() {
+		
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		setX(e.getX()-30);
+		setY(e.getY()-50);
+	}
+	
+}
+
+
+
+
--- src/_03_polymorphs/PolymorphWindow.java
+++ src/_03_polymorphs/PolymorphWindow.java
@@ -5,13 +5,15 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class PolymorphWindow extends JPanel implements ActionListener {
+public class PolymorphWindow extends JPanel implements ActionListener, MouseMotionListener {
 	public static final int WIDTH = 900;
 	public static final int HEIGHT = 600;
 
@@ -27,6 +29,7 @@ public class PolymorphWindow extends JPanel implements ActionListener {
 	public void buildWindow() {
 		window = new JFrame("IT'S MORPHIN' TIME!");
 		window.add(this);
+		window.addMouseMotionListener(this);
 		window.getContentPane().setPreferredSize(new Dimension(500, 500));
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.pack();
@@ -36,7 +39,8 @@ public class PolymorphWindow extends JPanel implements ActionListener {
 		pm.add(new RedMorph(50, 50, 50, 50));
 		pm.add(new MovingMorph(100, 100, 50, 50));
 		pm.add(new CircleMorph(300, 300, 50, 50));
-
+		pm.add(new MouseMorph(0,0,50,50));
+		
 		timer = new Timer(1000 / 30, this);
 		timer.start();
 	}
@@ -57,6 +61,23 @@ public class PolymorphWindow extends JPanel implements ActionListener {
 		repaint();
 		for (Polymorph p : pm) {
 			p.update();
+		}
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		// TODO Auto-generated method stub
+		for(Polymorph p : pm) {
+			if(p.getClass().equals(MouseMorph.class)) {
+				p.setX(e.getX()-30);
+				p.setY(e.getY()-50);
+			}
 		}
 	}
 }
